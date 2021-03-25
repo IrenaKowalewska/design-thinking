d05259d4cf5f5c143993140f024be00b1b011941
kleo4004 <zkioz4004@gmail.com>
2021-03-25 21:46:30 +0300

    feat:add slider

--- js/main.js
+++ js/main.js
@@ -26,5 +26,92 @@ const hamburgerMenu = () => {
     
 }
 
+const strategiesSlider = () => {
+    const slider = document.querySelector('.strategies__layout');
+    const slides = document.querySelectorAll('.strategy');
+    const strategyTitle = document.querySelectorAll('.strategy__title');
+    const slidesWrapper = document.querySelector('.strategies__layout');
+    const slidesField = document.querySelector('.strategies__layout-inner');
+    const slidesWrapperWidth = window.getComputedStyle(slidesWrapper).width;
+    let slideWidth = 348;
+    let itemMargin = 25;
+    slidesField.style.width = (slideWidth + itemMargin) * slides.length + 'px';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.6s all';
+    slidesWrapper.style.overflow = 'hidden';
+    slidesWrapper.style.position = 'relative';
+
+    let slideIndex = 1;
+    let offset = 0;
+    slides.forEach((slide, index) => {
+        slide.style.width = `${slideWidth}px`;
+        slide.setAttribute('data-slide-number', (index + 1))
+    });
+
+    const indicatorsWrapper = document.querySelector('.strategies__indicators');
+    let dotsArray = [];
+    const addSliderDots = () => {
+        for (let i = 0; i < slides.length; i++) {
+            const dot = document.createElement('span');
+            dot.setAttribute('data-slide-to', i + 1);
+            dot.classList.add('dot');
+            if(i === 0) {
+                dot.classList.add('dot_active');
+                strategyTitle[0].classList.add('strategy__title_active');
+            }
+            indicatorsWrapper.appendChild(dot);
+            dotsArray.push(dot);
+        }
+    }
+
+    const removeClass = (elements, className) => {
+        elements.forEach(element => {
+            element.classList.remove(className);
+        })
+    }
+
+    addSliderDots();
+
+    dotsArray.forEach(dot => {
+        dot.addEventListener('click', (event) => {
+            const slideTo = event.target.getAttribute('data-slide-to');
+            slideIndex = slideTo;
+            removeClass(strategyTitle, 'strategy__title_active');
+            removeClass(dotsArray, 'dot_active');
+            strategyTitle[slideTo - 1].classList.add('strategy__title_active');
+            dotsArray[slideTo - 1].classList.add('dot_active');
+            if (document.documentElement.offsetWidth < 370) {
+                slideWidth = 310;
+                slides.forEach(slide => {
+                    slide.style.width = `${slideWidth}px`;
+                });  
+            }
+            offset = (slideWidth + itemMargin) * (slideTo - 1);
+            if(document.documentElement.offsetWidth >= 1093) {
+                if(slides.length === 3) {
+                    slidesField.style.transform = `translateX(${0}px)`;
+                } else {
+                    slidesField.style.transform = `translateX(${-offset}px)`;
+                }
+                
+            } else {
+                slidesField.style.transform = `translateX(${-offset}px)`;
+            }
+            
+        })
+    });
+    
+    slides.forEach(slide => {
+        slide.addEventListener('click', () => {
+            const slideTo = slide.getAttribute('data-slide-number');
+            slideIndex = slideTo;
+            removeClass(strategyTitle, 'strategy__title_active');
+            removeClass(dotsArray, 'dot_active');
+            strategyTitle[slideTo - 1].classList.add('strategy__title_active');
+            dotsArray[slideTo - 1].classList.add('dot_active');
+        })
+    })
+}
 
 hamburgerMenu();
+strategiesSlider();
